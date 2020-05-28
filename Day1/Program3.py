75494d0f205a4ea5c11141bbd84c72016a202ae0
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-28 13:57:16 +0530

    Update Program3.py

--- Day1/Program3.py
+++ Day1/Program3.py
@@ -1,5 +1,5 @@
 #Program 3 of Tathastu-Week of Code by shaswat-99
-a=23
-b=56
+a=int(input("Enter 1st number"))
+b=int(input("Enter 2nd number"))
 a,b=b,a
 print(f"Swapped Value : a = {a} and b = {b}")
