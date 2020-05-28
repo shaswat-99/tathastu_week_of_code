358b25c4b44901bd671d8657e545b957e35334c7
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-28 14:21:14 +0530

    Update Program4.py

--- Day1/Program4.py
+++ Day1/Program4.py
@@ -3,5 +3,5 @@ cp=float(input("Enter Cost Price of the Product "))
 sp=float(input("Enter Selling Price of the Product "))
 profit=sp-cp
 print(f"Profit= {profit}")
-new_sp=((100+5)/100)*cp
-print(f" Selling Price after 5% profit on product = {new_sp}")
+new_sp=1.05*profit+cp
+print(f" Selling Price after 5% extra profit on product = {new_sp}")
