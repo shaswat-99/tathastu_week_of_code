4e54450ead02e561ab03d4a4207e0c5922172e75
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-28 13:55:25 +0530

    Update Program2.py

--- Day1/Program2.py
+++ Day1/Program2.py
@@ -1,4 +1,4 @@
 #Program 2 of Tathastu-Week of Code by shaswat-99
-num=int(input("Enter a Number "))
+num=float(input("Enter a Number "))
 sqr_root=num**0.5
-print(f"The square root of the given number is {sqr_root}")
+print(f"The square root of the given number {num} is {sqr_root}")
