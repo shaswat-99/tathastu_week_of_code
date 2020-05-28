b19921f2a227febb34a2d928a6f005fb6d5067f8
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-28 15:07:59 +0530

    Create Program5.py

---
+++ Day1/Program5.py
@@ -0,0 +1,16 @@
+#Program 5 of Tathastu-Week of Code by shaswat-99
+p1=int(input("Enter Runs of Player 1 in 60 balls "))
+p2=int(input("Enter Runs of Player 2 in 60 balls "))
+p3=int(input("Enter Runs of Player 3 in 60 balls "))
+sr1=(p1/60)*100
+sr2=(p2/60)*100
+sr3=(p3/60)*100
+print("Strike Rate of Player 1 =", sr1)
+print("Strike Rate of Player 2 =", sr2)
+print("Strike Rate of Player 3 =", sr3)
+print("Score of Player 1 if he played 60 more balls", p1*2)
+print("Score of Player 2 if he played 60 more balls", p2*2)
+print("Score of Player 3 if he played 60 more balls", p3*2)
+print("Number of sixes by Player 1 in 60 balls", p1//6)
+print("Number of sixes by Player 2 in 60 balls", p2//6)
+print("Number of sixes by Player 3 in 60 balls", p3//6)
