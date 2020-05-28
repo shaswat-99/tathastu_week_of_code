eac8e381b5dc0929a5908b20f3a5ba0df489a607
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-28 13:38:08 +0530

    Create Program1.py

---
+++ Day1/Program1.py
@@ -0,0 +1,11 @@
+#Program 1 of Tathastu-Week of Code by shaswat-99
+n1=input("Enter your name")
+n2=input("Enter your Branch")
+n3=input("Enter your Gender")
+n4=input("Enter your College name")
+n5=int(input("Enter your Age"))
+print("Name = ",n1)
+print("Branch = ",n2)
+print("Gender = ",n3)
+print("College = ",n4)
+print("Age = ",n5)
