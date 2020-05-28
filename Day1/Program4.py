ed5aec7b31b60dc13a057479ad4f0f7486e67130
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-28 14:13:54 +0530

    Create Program4.py

---
+++ Day1/Program4.py
@@ -0,0 +1,7 @@
+#Program 4 of Tathastu-Week of Code by shaswat-99
+cp=float(input("Enter Cost Price of the Product "))
+sp=float(input("Enter Selling Price of the Product "))
+profit=sp-cp
+print(f"Profit= {profit}")
+new_sp=((100+5)/100)*cp
+print(f" Selling Price after 5% profit on product = {new_sp}")
