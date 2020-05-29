68b4972bf24cd84cd121a465f9723a87f7993aff
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-29 17:20:06 +0530

    Update Program1.py

--- Day3/Program1.py
+++ Day3/Program1.py
@@ -1,6 +1,4 @@
 #Program1 of Tathastu-Week of Code by shaswat-99
 x=(input("Enter a String "))
-str=" "
-for i in x:
-    str=i+str
-print("Reversed String is",str)
+rev=x[::-1]
+print("Reversed String is",rev)
