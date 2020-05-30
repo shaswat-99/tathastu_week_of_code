befc5f4c991c3191e6412050d9183cb7ee32b0ec
Shaswat Priyam <66057549+shaswat-99@users.noreply.github.com>
2020-05-30 16:11:41 +0530

    Create Program3.py

---
+++ Day3/Program3.py
@@ -0,0 +1,5 @@
+#Program 3 of Tathastu-Week of Code by shaswat-99
+string=input("Enter a string: ")
+str_set=set(string)
+ans="".join(str_set)
+print(f"The string after removal of duplicate characters is: {ans}")
